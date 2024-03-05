b4e759665c2566b1d40547c49d1cc4f8cceeaeea
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-03-04 22:20:05 -0500

    random feature is working with only letters

--- password.py
+++ password.py
@@ -5,3 +5,7 @@ import random, string
 password = int(input("Password's Length: "))
 new_password =""
 
+for i in range(password):
+    new_password += random.choice(string.ascii_letters)
+
+print(new_password)
